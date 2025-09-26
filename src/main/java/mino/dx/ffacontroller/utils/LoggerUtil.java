f6a70fff9db2c1569c85ec39b66a3df8db3fa2b1
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2025-09-26 23:48:48 +0700

    Update LoggerUtil (pt. 3)

--- src/main/java/mino/dx/ffacontroller/utils/LoggerUtil.java
+++ src/main/java/mino/dx/ffacontroller/utils/LoggerUtil.java
@@ -9,47 +9,26 @@ import java.util.logging.Level;
 public class LoggerUtil {
     private static final FFAController plugin = JavaPlugin.getPlugin(FFAController.class);
 
-    @Deprecated
-    @SuppressWarnings("CallToPrintStackTrace")
-    public static void throwExceptionOld(String className, Exception e) {
-        String ctx = className + " has thrown an exception: " + e.getMessage();
-        plugin.getLogger().severe(ctx);
-        e.printStackTrace();
-    }
-
-    @Deprecated
-    @SuppressWarnings("unused")
-    public static void throwException(String className, Exception e) {
-        throwException(className, "has thrown an exception", e);
-    }
-
-    /**
-     * @deprecated chuyển đến dùng <code>LoggerUtil.severex(Throwable, String)</code>
-     * severex là phương thức mới viết từ severe (thêm chữ "x" đằng sau) thể hiện phương thức mới
-     * để dễ debug trong trường hợp các ngoại lệ được thả ra (ví dụ IOException...)
-     */
-    @Deprecated
-    public static void throwException(String className, String ctx, Exception e) {
-        plugin.getLogger().log(Level.SEVERE, className + " " + ctx, e); // ghép chuỗi className với ctx
-    }
-
-    @Deprecated
-    @SuppressWarnings("unused")
-    public static void warningException(String className, Exception e) {
-        warningException(className, "has thrown an exception", e);
-    }
-
-    @Deprecated
-    public static void warningException(String className, String ctx, Exception e) {
-        plugin.getLogger().log(Level.WARNING, className + " " + ctx, e);
-    }
-
     public static void log(String ctx) {
         plugin.getLogger().log(Level.INFO, ctx);
     }
 
     public static void log(String ctx, Throwable throwable) {
         plugin.getLogger().log(Level.INFO, ctx, throwable);
+    }
+
+    public static void logx(Throwable throwable, String ctx) {
+        StackTraceElement caller = Thread.currentThread().getStackTrace()[2];
+        // [0] = getStackTrace, [1] = throwException, [2] = nơi gọi LoggerUtil
+
+        String className = caller.getClassName();
+        String methodName = caller.getMethodName();
+
+        // Lấy tên class ngắn gọn, bỏ package
+        String simpleClassName = className.substring(className.lastIndexOf('.') + 1);
+        String location = simpleClassName + "#" + methodName;
+
+        plugin.getLogger().log(Level.INFO, location + " " + ctx, throwable);
     }
 
     public static void severe(String ctx) {
